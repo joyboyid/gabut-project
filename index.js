9477603c4f195a4b9407a934a21b132c820355f9
joyboyid <ican.notfound@gmail.com>
2024-06-20 16:11:31 +0700

    complete login

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
+import SequelizeStore from "connect-session-sequelize";
+
 import UserRoute from "./routes/UserRoute.js";
 import ProductRoute from "./routes/ProductRoute.js";
-// import db from './config/Database.js';
+import AuthRoute from "./routes/AuthRoute.js";
+import db from './config/Database.js';
 
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+const store = new sessionStore({
+  db: db
+});
 
 // (async()=>{
 //   await db.sync();
@@ -19,6 +27,7 @@ app.use(session({
   secret: process.env.SESS_SECRET,
   resave: false,
   saveUninitialized: true,
+  store: store,
   cookie:{
     secure: 'auto'
   }
@@ -31,6 +40,9 @@ app.use(cors({
 app.use(express.json());
 app.use(UserRoute);
 app.use(ProductRoute);
+app.use(AuthRoute);
+
+// store.sync();
 
 app.listen(process.env.APP_PORT, ()=> {
   console.log('Heads Up!');
